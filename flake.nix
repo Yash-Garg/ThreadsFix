6447a41d6fa6bf896f51c0f382e2c3442eff064c
Yash-Garg <yash.garg@outlook.in>
2023-07-07 01:46:23 +0530

    init: basic setup for workers rust

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell.${system} = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        ({pkgs, ...}: {
+          enterShell = ''
+            rustc --version
+          '';
+
+          packages = [
+            pkgs.pkg-config
+            pkgs.libressl
+            pkgs.nodejs_18
+          ];
+
+          languages.rust.enable = true;
+          languages.javascript.enable = true;
+        })
+      ];
+    };
+  };
+}
